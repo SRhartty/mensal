fd960de65a647e7aa8aeb2c140aaaa03c7b7178d
thierre <randonsspace@gmail.com>
2025-03-10 20:33:01 -0300

    initial commit

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Menu {
+
+    public void renderProduto() {
+        System.out.println(
+            "||========================================||\n" +
+            "||Sistema gerenciador de estock uniamerica||\n" +
+            "||========================================||\n" +
+            "||1 - Cadastrar produto                   ||\n" +
+            "||2 - Listar produtos                     ||\n" +
+            "||3 - Buscar por nome                     ||\n" +
+            "||4 - Deletar produto                     ||\n" +
+            "||5 - Atualizar produto                   ||\n" +
+            "||6 - Sair                                ||\n" +
+            "||========================================||\n");
+    }
+}
