94811702211aba1c2259b5c5bb2e7ab1be21e335
SRhartty <randonsspace@gmail.com>
2025-03-11 23:16:18 -0300

    cliente

--- src/main/java/org/example/interfaces/ClienteInterface.java
+++ src/main/java/org/example/interfaces/ClienteInterface.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import org.example.clientes.Cliente;
 
 public interface ClienteInterface {
-    void createCliente(Map<String, Cliente> clientes, Cliente cliente, Scanner scanner);
+    void createCliente(Map<String, Cliente> clientes, Cliente cliente);
     void getCliente(Map<String, Cliente> clientes);
     void findByName(Map<String, Cliente> clientes, Scanner scanner);
     void deleteCliente(Map<String, Cliente> clientes, Scanner scanner);
--- src/main/java/org/example/menu/Menu.java
+++ src/main/java/org/example/menu/Menu.java
@@ -9,7 +9,7 @@ public class Menu {
                         ||Sistema gerenciador de estock uniamerica||
                         ||========================================||
                         ||1 - Produtos                            ||
-                        ||2 - Usuários                            ||
+                        ||2 - Cliente                            ||
                         ||3 - Pdv                                 ||
                         ||4 - Sair                                ||
                         ||========================================||
@@ -28,6 +28,22 @@ public class Menu {
                         ||3 - Buscar por nome                     ||
                         ||4 - Deletar produto                     ||
                         ||5 - Atualizar produto                   ||
+                        ||6 - Voltar                              ||
+                        ||========================================||
+                        """);
+    }
+
+    public void renderCliente() {
+        System.out.println(
+                """
+                        ||========================================||
+                        ||Sistema gerenciador de estock uniamerica||
+                        ||========================================||
+                        ||1 - Cadastrar cliente                   ||
+                        ||2 - Listar clientes                     ||
+                        ||3 - Buscar por nome                     ||
+                        ||4 - Deletar cliente                     ||
+                        ||5 - Atualizar cliente                   ||
                         ||6 - Voltar                              ||
                         ||========================================||
                         """);
