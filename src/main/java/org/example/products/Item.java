fae86f74eafc8debbc596facdfa51689892f8a8c
SRhartty <randonsspace@gmail.com>
2025-03-11 23:22:57 -0300

    try cath

--- src/main/java/org/example/products/Item.java
+++ src/main/java/org/example/products/Item.java
@@ -50,32 +50,36 @@ public class Item extends Stock implements ItemInterface {
     @Override
     public void createItem(Map<String, Item> products, Item item) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nome: ");
-        String nome = scanner.next();
-        System.out.print("Preço: ");
-        float preco = scanner.nextFloat();
-        System.out.print("Quantidade: ");
-        int quantidade = scanner.nextInt();
-        System.out.print("Categoria (LIVRE | RESTRITO): ");
-        Categoria categoria = Categoria.valueOf(scanner.next().toUpperCase());
-        Item newItem = new Item(nome, preco, quantidade, categoria);
-        if (products.containsKey(newItem.getNome())) {
-            System.out.println("Produto já existe!");
-        } else {
-            products.put(newItem.getNome(), newItem);
-            System.out.println("Produto cadastrado com sucesso!");
-            System.out.println("||========================================||");
-        }
-        String answer;
-        System.out.println("Cadastrar outro produto? (S/N)");
-        answer = scanner.next();
-        while (answer.equalsIgnoreCase("S")) {
-            if (answer.equalsIgnoreCase("N")) {
-                break;
+        try{
+            System.out.print("Nome: ");
+            String nome = scanner.next();
+            System.out.print("Preço: ");
+            float preco = scanner.nextFloat();
+            System.out.print("Quantidade: ");
+            int quantidade = scanner.nextInt();
+            System.out.print("Categoria (LIVRE | RESTRITO): ");
+            Categoria categoria = Categoria.valueOf(scanner.next().toUpperCase());
+            Item newItem = new Item(nome, preco, quantidade, categoria);
+            if (products.containsKey(newItem.getNome())) {
+                System.out.println("Produto já existe!");
+            } else {
+                products.put(newItem.getNome(), newItem);
+                System.out.println("Produto cadastrado com sucesso!");
+                System.out.println("||========================================||");
             }
-            item.createItem(products, item);
+            String answer;
             System.out.println("Cadastrar outro produto? (S/N)");
             answer = scanner.next();
+            while (answer.equalsIgnoreCase("S")) {
+                if (answer.equalsIgnoreCase("N")) {
+                    break;
+                }
+                item.createItem(products, item);
+                System.out.println("Cadastrar outro produto? (S/N)");
+                answer = scanner.next();
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar produto.");
         }
     }
 
@@ -96,16 +100,21 @@ public class Item extends Stock implements ItemInterface {
     public void findByName(Map<String, Item> products, Scanner scanner) {
         System.out.println("Buscar por nome");
         System.out.print("Digite o nome do produto: ");
-        String name = scanner.next();
-        Item product = products.get(name);
-        if (product != null) {
-            System.out.println("Nome: " + product.getNome());
-            System.out.println("Preço: " + product.getPreco());
-            System.out.println("Quantidade: " + product.getQuantidade());
-            System.out.println("Categoria: " + product.getCategoria());
-            System.out.println("||=========================================||");
-        } else {
-            System.out.println("Produto não encontrado.");
+        try {
+            String name = scanner.next();
+
+            Item product = products.get(name);
+            if (product != null) {
+                System.out.println("Nome: " + product.getNome());
+                System.out.println("Preço: " + product.getPreco());
+                System.out.println("Quantidade: " + product.getQuantidade());
+                System.out.println("Categoria: " + product.getCategoria());
+                System.out.println("||=========================================||");
+            } else {
+                System.out.println("Produto não encontrado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar produto.");
         }
     }
 
@@ -113,13 +122,17 @@ public class Item extends Stock implements ItemInterface {
     public void deleteProduct(Map<String, Item> products, Scanner scanner) {
         System.out.println("Deletar produto");
         System.out.print("Digite o nome do produto: ");
-        String deleteName = scanner.next();
-        Item productDelete = products.get(deleteName);
-        if (productDelete != null) {
-            products.remove(deleteName);
-            System.out.println("Produto deletado com sucesso!");
-        } else {
-            System.out.println("Produto não encontrado.");
+        try {
+            String deleteName = scanner.next();
+            Item productDelete = products.get(deleteName);
+            if (productDelete != null) {
+                products.remove(deleteName);
+                System.out.println("Produto deletado com sucesso!");
+            } else {
+                System.out.println("Produto não encontrado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao deletar produto.");
         }
     }
 
@@ -127,20 +140,24 @@ public class Item extends Stock implements ItemInterface {
     public void updateProduct(Map<String, Item> products, Scanner scanner) {
         System.out.println("Atualizar produto");
         System.out.print("Digite o nome do produto a ser atualizado: ");
-        String updateName = scanner.next();
-        Item updateProduct = products.get(updateName);
-        if (updateProduct != null) {
-            System.out.print("Digite o novo nome: ");
-            updateProduct.setNome(scanner.next());
-            System.out.print("Digite o novo preço: ");
-            updateProduct.setPreco(scanner.nextDouble());
-            System.out.print("Digite a nova quantidade: ");
-            updateProduct.setQuantidade(scanner.nextInt());
-            System.out.print("Digite a nova categoria: ");
-            updateProduct.setCategoria(Categoria.valueOf(scanner.next().toUpperCase()));
-            System.out.println("Produto atualizado com sucesso!");
-        } else {
-            System.out.println("Produto não encontrado.");
+        try {
+            String updateName = scanner.next();
+            Item updateProduct = products.get(updateName);
+            if (updateProduct != null) {
+                System.out.print("Digite o novo nome: ");
+                updateProduct.setNome(scanner.next());
+                System.out.print("Digite o novo preço: ");
+                updateProduct.setPreco(scanner.nextDouble());
+                System.out.print("Digite a nova quantidade: ");
+                updateProduct.setQuantidade(scanner.nextInt());
+                System.out.print("Digite a nova categoria: ");
+                updateProduct.setCategoria(Categoria.valueOf(scanner.next().toUpperCase()));
+                System.out.println("Produto atualizado com sucesso!");
+            } else {
+                System.out.println("Produto não encontrado.");
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao atualizar produto.");
         }
     }
 
